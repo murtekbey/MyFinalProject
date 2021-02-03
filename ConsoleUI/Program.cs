bc56bdabd3d9dd2cec53adfd345c9e5042973850
Murat Altınpınar <murtekbey@gmail.com>
2021-02-03 23:47:31 +0300

    03.02.2021 end of class

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,19 +1,29 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using System;
 
 namespace ConsoleUI
 {
+    // SOLID
+    // Open Closed Principle
     class Program
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAll())
+            //foreach (var product in productManager.GetAll())
+            //{
+            //    Console.WriteLine(product);
+            //}
+
+            foreach (var item in productManager.GetAllByCategoryId(2))
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine("{0} - {1}",item.ProductName, item.CategoryId);
             }
+
+
         }
     }
 }
--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
+// generic constraint
+// class : reference type
+// IEntity : IEntity olabilir veya IEntity implemente eden bir nesne olabilir.
+// new() : new'lenebilir olmalı (interface new'lenemez)
 namespace DataAccess.Abstract
 {
-    public interface IEntityRepository<T>
+    public interface IEntityRepository<T> where T:class,IEntity,new()
     {
         List<T> GetAll(Expression<Func<T, bool>> filter=null);
         T Get(Expression<Func<T, bool>> filter);
