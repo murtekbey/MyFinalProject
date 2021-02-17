5eec57add06ffb92c1cc145e3c7c206f798607ae
Murat Altınpınar <murtekbey@gmail.com>
2021-02-18 00:11:25 +0300

    17 february end of lesson

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,9 +1,13 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -19,14 +23,11 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
-        //[LogAspect] --> AOP
+        //validation
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-            if (product.ProductName.Length<2)
-            {
-                // magic strings
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
+            //business codes
             _productDal.Add(product);
 
             return new SuccessResult(Messages.ProductAdded);
