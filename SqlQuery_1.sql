70c696152a9c182ff6c481ce30c4d613d4c5c717
Murat Altınpınar <murtekbey@gmail.com>
2021-03-01 02:01:51 +0300

    SQL Query File is added

---
+++ SqlQuery_1.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]           INT          IDENTITY (1, 1) NOT NULL,
+    [FirstName]    VARCHAR (50) NOT NULL,
+    [LastName]     VARCHAR (50) NOT NULL,
+    [Email]        VARCHAR (50) NOT NULL,
+    [PasswordHash] BINARY (128) NOT NULL,
+    [PasswordSalt] BINARY (128) NOT NULL,
+    [Status]       BIT          NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[OperationClaims] (
+    [Id]   INT           IDENTITY (1, 1) NOT NULL,
+    [Name] VARCHAR (250) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[UserOperationClaims] (
+    [Id]               INT IDENTITY (1, 1) NOT NULL,
+    [UserId]           INT NOT NULL,
+    [OperationClaimId] INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id]),
+    FOREIGN KEY ([OperationClaimId]) REFERENCES [dbo].[OperationClaims] (Id),
+);
